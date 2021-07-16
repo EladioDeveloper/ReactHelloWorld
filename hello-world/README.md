22535ee05d183856ac0dd23c8d9f63c720405049
Eladiodeveloper <eladiojuniorrodriguez@gmail.com>
2021-07-15 23:35:23 -0400

    Initial Commit

--- hello-world/README.md
+++ hello-world/README.md
@@ -11,3 +11,17 @@
 
     * Docker
     * Python 3
+
+## Tests
+
+Run unittests + Coverage
+
+```
+    coverage run my-unittest.py
+    coverage report -m --fail-under=89 #falla sino llegamos al 80%
+```
+
+## Run Integration Tests
+```
+behave tests/Integracion
+```
--- hello-world/calc.py
+++ hello-world/calc.py
@@ -1,5 +1,8 @@
-class Calc():
+class Calc:
     def sumar(self, num1, num2):
-        if(num1  < 0 or num2 < 0):
-            return "Invalid"
-        return num1 + num2
+        try:
+            if(num1  < 0 or num2 < 0):
+                return "Invalid"
+            return num1 + num2
+        except Exception:
+            return "Excepcion"
